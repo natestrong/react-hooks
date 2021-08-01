95059a3d513da9a346cd75b04c0ba6732b2f9f40
nstrong <nathanstrong.fx@gmail.com>
2021-08-01 16:47:56 -0700

    hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,18 +4,18 @@
 import * as React from 'react';
 
 function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = '';
+  const [name, setName] = React.useState('Nathan');
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    const name = event.target.value;
+    setName(name);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
