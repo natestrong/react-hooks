017705ffd8c52ec1ad09ff96b57509350c0d0ef5
nstrong <nathanstrong.fx@gmail.com>
2021-08-01 16:19:44 -0700

    hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,11 +1,11 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import * as React from 'react';
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const name = '';
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
@@ -19,11 +19,11 @@ function Greeting() {
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting />;
 }
 
-export default App
+export default App;
